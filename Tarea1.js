90006f84ad81fb073cc85351c2b99bb551a59033
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-04-30 16:51:28 -0400

    Intento 1 de preguntar al usuario

--- Tarea1.js
+++ Tarea1.js
@@ -1,3 +1,4 @@
+
 function fibonacci(x) {
     if(x<=1) {
         return x;
@@ -5,4 +6,14 @@ function fibonacci(x) {
     return fibonacci(x-1)+fibonacci(x-2);
 }
 
-console.log(fibonacci(5))
+console.log("Escriba el número para la suceción:");
+var stdin = process.openStdin();
+
+
+stdin.addListener("data", function(d) {
+    console.log("F(" + d.toString().trim() +")");
+});
+
+
+
+console.log(fibonacci(8))
